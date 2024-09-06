f8c896dba1a0d55110870b24831158afba2f5256
Elçin  KARADENİZ <148655453+elcinkaradeniz@users.noreply.github.com>
2024-09-06 20:13:41 +0300

    Navbar.jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -25,10 +25,10 @@ const Navbar = () => {
      </div>
      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
      <ul ref={menuRef}  className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}}  to='/'>Shop</Link>{menu=="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}} ><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>  {menu=="mens"?<hr/>:<></>}     </li>
-        <li onClick={()=>{setMenu("womens")}} ><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu=="womens"?<hr/>:<></>}  </li>
-        <li onClick={()=>{setMenu("kids")}} ><Link style={{textDecoration: 'none'}}  to='/kids'>Kids</Link> {menu=="kids"?<hr/>:<></>}   </li>
+        <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}}  to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}} ><Link style={{textDecoration: 'none'}} to='/mens'>Men</Link>  {menu==="mens"?<hr/>:<></>}     </li>
+        <li onClick={()=>{setMenu("womens")}} ><Link style={{textDecoration: 'none'}} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}  </li>
+        <li onClick={()=>{setMenu("kids")}} ><Link style={{textDecoration: 'none'}}  to='/kids'>Kids</Link> {menu==="kids"?<hr/>:<></>}   </li>
      </ul>
      <div className="nav-login-cart">
       {localStorage.getItem('auth-token')
